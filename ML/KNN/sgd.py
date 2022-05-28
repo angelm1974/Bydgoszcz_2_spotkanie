1111f9b2d3c1389fe84fd0d399b7a6b5a422c38d
Mariusz Mol <mariusz980@gmail.com>
2022-05-28 14:36:08 +0200

    sd

---
+++ ML/KNN/sgd.py
@@ -0,0 +1,6 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.datasets import make_blobs
+import matplotlib.pyplot as plt
+import numpy as np
+import argparse
